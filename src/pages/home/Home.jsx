107360f8305d2b8ef5f248379a5aa1b8844f617a
roushan-sheik <roushansheik@gmail.com>
2024-04-09 07:36:46 +0600

    json data updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
+import { Button } from "flowbite-react";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+
 const Home = () => {
   const { data } = useLoaderData();
   console.log(data);
-  return <div>Home</div>;
+  return (
+    
+  );
 };
 
 export default Home;
