0c5ef04eb63282f14d85934b87ff58ac007324ee
roushan-sheik <roushansheik@gmail.com>
2024-04-09 22:37:07 +0600

    real states card added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import { stateTitle } from "../../constant/title";
+import State from "../../components/state/State";
 import Title from "../../components/title/Title";
+import { stateTitle } from "../../constant/title";
 const Home = () => {
   const { data } = useLoaderData();
-  console.log(data);
+
   return (
     <div>
       <Title title={stateTitle.title} description={stateTitle.description} />
-      <div></div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {data.map((state) => (
+          <State key={state.id} stateData={state} />
+        ))}
+      </div>
     </div>
   );
 };
