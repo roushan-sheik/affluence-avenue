de9fcc9fac3859953d0c0f3904262175a1fcf778
roushan-sheik <roushansheik@gmail.com>
2024-04-11 20:53:27 +0600

    register form updated

--- src/components/input/Inp.jsx
+++ src/components/input/Inp.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 
 const Inp = ({
-  style,
   name,
   onClick,
-  label,
   labelStyle,
   placeholder,
-  defaultValue,
-  object,
+  value,
+  label,
+  type,
+  inputStyle,
+  onChange,
+  required,
+  boxStyle,
 }) => {
   return (
-    <div className="relative w-max rounded-lg">
-      <input
-        className={`peer rounded-lg border border-[#1B8EF8] bg-transparent px-4 py-2 text-[#1B8EF8] focus:outline-none ${style}`}
-        type="text"
-        name={name}
-        placeholder={placeholder}
-        defaultValue={defaultValue}
-        onClick={onClick}
-        {...object}
-        id="navigate_ui_input_33"
-      />
-      <label
-        className={`absolute -top-2 left-[10px] rounded-md px-2 text-xs text-slate-400 duration-300 peer-placeholder-shown:left-[14px] peer-placeholder-shown:top-3  peer-placeholder-shown:bg-transparent peer-placeholder-shown:text-sm peer-focus:-top-2 peer-focus:left-[10px] peer-focus:bg-sky-300 peer-focus:text-xs peer-focus:text-sky-800 dark:peer-focus:text-sky-400 dark:peer-focus:bg-[#0F172A] ${labelStyle} `}
-        htmlFor="navigate_ui_input_33"
-      >
-        {label}
+    <div className={`${boxStyle} flex flex-col gap-2`}>
+      <label className={`${labelStyle} font-semibold`} htmlFor={name}>
+        {" "}
+        {label}:
       </label>
+      <input
+        onChange={onChange}
+        placeholder={placeholder}
+        onClick={onClick}
+        value={value}
+        required={required}
+        name={name}
+        id={name}
+        className={`rounded-lg border border-sky-500 bg-transparent px-4 py-2 text-[#1B8EF8] focus:outline-none ${inputStyle} `}
+        type={type}
+      />
     </div>
   );
 };
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,130 +1,63 @@
-import { Label, TextInput } from "flowbite-react";
-import { useFormik } from "formik";
 import React from "react";
 import { Link } from "react-router-dom";
-import * as yup from "yup";
 import Btn from "../../components/button/Btn";
-import useUserContext from "../../hooks/useUserContext";
+import Inp from "../../components/input/Inp";
 
 const Register = () => {
-  const { createUser } = useUserContext();
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      photo: "",
-      password: "",
-    },
-    // We have a validation schema with formik to validate our field
-    validationSchema: yup.object({
-      name: yup.string().required().min(3),
-      email: yup.string().email().required(),
-      photo: yup.string().required().url(),
-      password: yup.string().min(8).required(),
-    }),
-    onSubmit: (value) => {
-      const { email, password } = value;
-      createUser(email, password)
-        .then((result) => {
-          console.log(result.user);
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-    },
+  const [user, setUser] = React.useState({
+    name: "",
+    email: "",
+    photoUrl: "",
+    password: "",
   });
-
-  // name validaton
-  let renderNameErrors = formik.touched.name && formik.errors.name && (
-    <span className="text-red-500">{formik.errors.name}</span>
-  );
-  // email validaton
-  let renderEmailErrors = formik.touched.email && formik.errors.email && (
-    <span className="text-red-500">{formik.errors.email}</span>
-  );
-
-  // photo url validaton
-  let renderPhotoErrors = formik.touched.photo && formik.errors.photo && (
-    <span className="text-red-500">{formik.errors.photo}</span>
-  );
-
-  // password validaton
-  let renderPasswordErrors = formik.touched.password &&
-    formik.errors.password && (
-      <span className="text-red-500">{formik.errors.password}</span>
-    );
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+  // handle the form
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(user);
+  }
   return (
     <div className="flex flex-col justify-center items-center">
-      <h2 className=" text_pri text-4xl my-4 font-bold text-center">
-        Register
-      </h2>
+      <h2 className=" text_pri text-4xl my-4 font-bold text-center">LogIn</h2>
       <form
-        onSubmit={formik.handleSubmit}
+        onSubmit={handleSubmit}
         className="flex max-w-md flex-col md:w-[50%] w-[94%] gap-4"
+        action="#"
       >
-        {/* name  */}
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="name" value="Your name" />
-          </div>
-          <TextInput
-            id="name"
-            type="text"
-            onChange={formik.handleChange}
-            required
-            shadow
-            name="name"
-          />
-        </div>
-        {renderNameErrors}
-        {/* email  */}
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="email2" value="Your email" />
-          </div>
-          <TextInput
-            id="email2"
-            type="email"
-            placeholder="name@flowbite.com"
-            onChange={formik.handleChange}
-            required
-            shadow
-            name="email"
-          />
-        </div>
-        {renderEmailErrors}
-        {/* photo url  */}
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="photo" value="Photo Url" />
-          </div>
-          <TextInput
-            id="photo"
-            type="text"
-            placeholder="http://arifamoni.com/profile.png"
-            onChange={formik.handleChange}
-            required
-            shadow
-            name="photo"
-          />
-        </div>
-        {renderPhotoErrors}
-        {/* password  */}
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="password" value="Your password" />
-          </div>
-          <TextInput
-            id="password"
-            type="password"
-            required
-            onChange={formik.handleChange}
-            shadow
-            autoComplete="on"
-            name="password"
-          />
-        </div>
-        {renderPasswordErrors}
+        {/* inputs  */}
+        <Inp
+          type="text"
+          name={"name"}
+          required={true}
+          label={"Name"}
+          placeholder={"name"}
+          onChange={handleChange}
+        />
+        <Inp
+          type="text"
+          name={"email"}
+          label={"Email"}
+          required={true}
+          placeholder={"email"}
+          onChange={handleChange}
+        />
+        <Inp
+          type="text"
+          name={"photoUrl"}
+          label={"Photo Url"}
+          required={true}
+          placeholder={"http://server/arifamoni.jpg"}
+          onChange={handleChange}
+        />
+        <Inp
+          type="password"
+          name={"password"}
+          required={true}
+          label={"Password"}
+          onChange={handleChange}
+        />
         {/* submit button  */}
         <Btn type={"submit"}> Register</Btn>
         <p className=" text-base text_sec text-center ">
