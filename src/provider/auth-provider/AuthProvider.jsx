0484dc717d328733ff78ff5a3f3656bc6474b1df
roushan-sheik <roushansheik@gmail.com>
2024-04-12 10:08:43 +0600

    on auth state changed get current login user

--- src/provider/auth-provider/AuthProvider.jsx
+++ src/provider/auth-provider/AuthProvider.jsx
@@ -1,12 +1,13 @@
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import React from "react";
 import auth from "./../../services/firebase";
 export const authContext = React.createContext(null);
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = React.useState("user nai");
+  const [user, setUser] = React.useState(null);
   //  create user with email and password
   function createUser(email, password) {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -15,8 +16,15 @@ const AuthProvider = ({ children }) => {
   function loginUser(email, password) {
     return signInWithEmailAndPassword(auth, email, password);
   }
-  // get user
-  // onAuthStateChanged(auth)
+  // on auth state changed get current login user
+  React.useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   // authentication user info data
   const userInfo = { user, createUser, loginUser };
   return (
